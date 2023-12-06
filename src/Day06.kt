6099dd54875b677f0c4385e523c9d2901e8e21c1
Paulo Cezar <paulocezar.ufg@gmail.com>
2023-12-06 11:24:33 +0100

    day 6 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,19 +1,23 @@
 fun main() {
     val d = Day06()
     d.part1().println()
+    d.part2().println()
 }
 
 class Day06 {
     private val input: List<String> = readInput(this::class.simpleName!!)
-    private val races: List<Race> = buildList {
+    private val smallRaces: List<Race> = buildList {
         val times = input[0].split(':')[1].toIntList()
-        val distances = input[1].split(':')[1].toIntList()
+        val distances = input[1].split(':')[1].toLongList()
         times.zip(distances) { time, distance -> add(Race(time, distance))}
     }
+    private val fixKerning = { str: String -> str.filter { c -> c.isDigit() } }
+    private val bigRace = Race(fixKerning(input[0]).toInt(), fixKerning(input[1]).toLong())
 
-    fun part1() = races.fold(1) { ways, race -> ways * race.waysToWin() }
+    fun part1() = smallRaces.fold(1) { totalWays, race -> totalWays * race.waysToWin() }
+    fun part2() = bigRace.waysToWin()
 
-    data class Race(val time: Int, val distance: Int) {
-        fun waysToWin(): Int = (0..time).count { charge -> (time-charge)*charge > distance }
+    data class Race(val time: Int, val distance: Long) {
+        fun waysToWin(): Int = (1..<time).count { charge -> (time-charge) > distance/charge }
     }
 }
