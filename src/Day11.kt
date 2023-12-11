623adeee223a44d107bbb5e1386d1622226c9705
Paulo Cezar <paulocezar.ufg@gmail.com>
2023-12-11 13:50:40 +0100

    day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -2,21 +2,25 @@ import kotlin.math.abs
 fun main() {
     val d = Day11()
     d.part1().println()
+    d.part2().println()
 }
 
 class Day11 {
     private val input: List<String> = readInput(this::class.simpleName!!)
-    private val galaxyPositions = getExpandedGalaxyPositions()
 
-    fun part1() = galaxyPositions.foldIndexed(0) { index, sumDistances, galaxy1 ->
-        sumDistances + galaxyPositions.subList(index+1, galaxyPositions.size).fold(0) { g1Distances, galaxy2 ->
-            g1Distances + distance(galaxy1, galaxy2)
+    fun part1() = sumPairwiseDistances(getExpandedGalaxyPositions())
+    fun part2() = sumPairwiseDistances(getExpandedGalaxyPositions(1_000_000))
+
+    private fun sumPairwiseDistances(galaxyPositions: List<Pair<Int,Int>>) = galaxyPositions.foldIndexed(0L)
+        { index, sumDistances, galaxy1 ->
+            sumDistances + galaxyPositions.subList(index+1, galaxyPositions.size).fold(0L)
+                { g1Distances, galaxy2 -> g1Distances + distance(galaxy1, galaxy2) }
         }
-    }
 
-    private fun distance(g1: Pair<Int, Int>, g2: Pair<Int,Int>) = abs(g1.first - g2.first) + abs(g1.second - g2.second)
+    private fun distance(g1: Pair<Int, Int>, g2: Pair<Int,Int>) =
+            abs(g1.first - g2.first) + abs(g1.second - g2.second)
 
-    private fun getExpandedGalaxyPositions() : List<Pair<Int,Int>> = buildList {
+    private fun getExpandedGalaxyPositions(expansionFactor: Int = 2) : List<Pair<Int,Int>> = buildList {
         val emptyColumns = input[0].indices.filter { column ->
             input.all { it[column] == '.' }
         }
@@ -29,9 +33,9 @@ class Day11 {
                     isEmpty = false
                     add(Pair(actualRow, actualColumn))
                 }
-                actualColumn += if (emptyColumns.binarySearch(index) >= 0) 2 else 1
+                actualColumn += if (emptyColumns.binarySearch(index) >= 0) expansionFactor else 1
             }
-            actualRow += if (isEmpty) 2 else 1
+            actualRow += if (isEmpty) expansionFactor else 1
         }
     }
 }
