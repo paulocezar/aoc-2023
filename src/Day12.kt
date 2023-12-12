8e2c546e2e9f72a84ae89d79c12a925ac3aec8df
Paulo Cezar <paulocezar.ufg@gmail.com>
2023-12-12 16:23:07 +0100

    day 12 part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -2,42 +2,53 @@ import kotlin.math.min
 fun main() {
     val d = Day12()
     d.part1().println()
+    d.part2().println()
 }
 
 class Day12 {
     private val input: List<String> = readInput(this::class.simpleName!!)
 
-    fun part1() = input.map { Record.parse(it).countArrangements() }.reduce(Int::plus)
+    fun part1() = input.map { Record.parse(it).countArrangements() }.reduce(Long::plus)
+    fun part2() = input.map { Record.parseUnfolded(it).countArrangements() }.reduce(Long::plus)
 
     data class Record(val condition: String, val brokenGroups: List<Int>) {
+        private val cache = mutableMapOf<Triple<Int,Int,Int>, Long>()
         fun countArrangements() = count(0, 0, 0)
 
-        private fun count(spring: Int, broken: Int, group: Int) : Int {
+        private fun count(spring: Int, broken: Int, group: Int) : Long {
             if (group == brokenGroups.size) return if (condition.lastIndexOf('#') >= spring) 0 else 1
             if (spring == condition.length) return if (broken == brokenGroups[group]) count(spring, 0, group+1) else 0
-            val options = when (condition[spring]) {
-                '#' -> "#"
-                '.' -> "."
-                '?' -> "#."
-                else -> throw Exception("unexpected spring condition ${condition[spring]}")
-            }
-            var res = 0
-            options.forEach { state ->
-                if (state == '.') {
-                    if (broken == 0 || broken == brokenGroups[group])
-                        res += count(spring+1, 0, group + min(broken, 1))
-                } else {
-                    if (broken+1 <= brokenGroups[group])
-                        res += count(spring+1, broken+1, group)
+            return cache.getOrPut(Triple(spring,broken,group)) {
+                val options = when (condition[spring]) {
+                    '#' -> "#"
+                    '.' -> "."
+                    '?' -> "#."
+                    else -> throw Exception("unexpected spring condition ${condition[spring]}")
                 }
+                var res = 0L
+                options.forEach { state ->
+                    if (state == '.') {
+                        if (broken == 0 || broken == brokenGroups[group])
+                            res += count(spring+1, 0, group + min(broken, 1))
+                    } else {
+                        if (broken+1 <= brokenGroups[group])
+                            res += count(spring+1, broken+1, group)
+                    }
+                }
+                res
             }
-            return res
         }
 
         companion object {
             fun parse(input: String): Record {
                 val (condition, groups) = input.split(' ')
                 return Record(condition, groups.toIntList(','))
+            }
+            fun parseUnfolded(input: String): Record {
+                val (condition, groups) = input.split(' ')
+                val unfoldedCondition = List(5) { condition }.joinToString("?")
+                val unfoldedGroups = List(5) { groups }.joinToString(",")
+                return Record(unfoldedCondition, unfoldedGroups.toIntList(','))
             }
         }
     }
