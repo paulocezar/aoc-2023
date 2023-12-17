f4043cce4d8c1162045e600cc1162483d71bc99b
Paulo Cezar <paulocezar.ufg@gmail.com>
2023-12-17 04:11:25 +0100

    day 16 part 2

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,15 +1,28 @@
+import kotlin.math.max
 fun main() {
     val d = Day16()
     d.part1().println()
+    d.part2().println()
 }
 
 typealias Position = Pair<Int, Int>
 class Day16 {
     private val input: List<String> = readInput(this::class.simpleName!!)
 
-    fun part1() = let {
-        visit(Position(0, 0), Direction.Right)
-        visited.map { it.first }.toSet().size
+    fun part1() = countVisited(Position(0, 0), Direction.Right)
+    fun part2() = let {
+        var maxEnergized = 0
+        for (row in input.indices) {
+            for ((column, dir) in listOf(Pair(0, Direction.Right), Pair(input[0].length-1, Direction.Left))) {
+                maxEnergized = max(maxEnergized, countVisited(Position(row, column), dir))
+            }
+        }
+        for (column in input[0].indices) {
+            for ((row, dir) in listOf(Pair(0, Direction.Down), Pair(input.size-1, Direction.Top))) {
+                maxEnergized = max(maxEnergized, countVisited(Position(row, column), dir))
+            }
+        }
+        maxEnergized
     }
 
     enum class Direction { Right, Left, Top, Down }
@@ -54,13 +67,21 @@ class Day16 {
         Direction.Down -> Position(first+1, second)
     }
 
-    private val visited = mutableSetOf<Pair<Position, Direction>>()
-    private fun visit(position: Position, direction: Direction) {
-        if (!(position.first in input.indices && position.second in input[0].indices)) return
-        if (!visited.add(Pair(position, direction))) return
-        val movesTo = GridCell.fromValue(input[position.first][position.second]).next(direction)
-        for (dir in movesTo) {
-            visit(position.move(dir), dir)
+    private fun countVisited(initialPosition: Position, initialDirection: Direction) : Int {
+        val visited = mutableSetOf<Pair<Position, Direction>>()
+        val queue = ArrayDeque<Pair<Position, Direction>>()
+        visited.add(Pair(initialPosition, initialDirection))
+        queue.addLast(Pair(initialPosition, initialDirection))
+        while (!queue.isEmpty()) {
+            val (pos, dir) = queue.removeFirst()
+            val movesTo = GridCell.fromValue(input[pos.first][pos.second]).next(dir)
+            for (nextDir in movesTo) {
+                val nextPos = pos.move(nextDir)
+                if (!(nextPos.first in input.indices && nextPos.second in input[0].indices)) continue
+                if (!visited.add(Pair(nextPos, nextDir))) continue
+                queue.addLast(Pair(nextPos, nextDir))
+            }
         }
+        return visited.map { it.first }.toSet().size
     }
 }
