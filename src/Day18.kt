bd4db8b96bf9c07aef659623eea8bc6afc0f7182
Paulo Cezar <paulocezar.ufg@gmail.com>
2023-12-18 12:21:32 +0100

    day 18 part 2

--- src/Day18.kt
+++ src/Day18.kt
@@ -3,22 +3,35 @@ import kotlin.math.abs
 fun main() {
     val d = Day18()
     d.part1().println()
+    d.part2().println()
 }
 
 class Day18 {
     private val input: List<String> = readInput(this::class.simpleName!!)
     private val regex = """(\w) (\d+) \(#(\w+)\)""".toRegex()
     private val plan = DigPlan(input.map {
-        val (dir, dist, color) = regex.matchEntire(it)!!.destructured
-        DigInstruction(dir[0], dist.toInt(), color)
+        val (dir, dist, _) = regex.matchEntire(it)!!.destructured
+        DigInstruction(dir[0], dist.toInt())
+    })
+    private val actualPlan = DigPlan(input.map {
+        val (_, _, hex) = regex.matchEntire(it)!!.destructured
+        val dir = when (hex.last()) {
+            '0' -> 'R'
+            '1' -> 'D'
+            '2' -> 'L'
+            '3' -> 'U'
+            else -> throw Exception("unexpected digit ${hex.last()}")
+        }
+        DigInstruction(dir, hex.dropLast(1).toInt(16))
     })
 
     fun part1() = plan.getArea()
+    fun part2() = actualPlan.getArea()
 
-    data class DigInstruction(val direction: Char, var distance: Int, val color: String)
+    data class DigInstruction(val direction: Char, var distance: Int)
     data class DigPlan(val instructions: List<DigInstruction>) {
-        fun getArea() : Int {
-            var curVertex = Pair(0, 0)
+        fun getArea() : Long {
+            var curVertex = Pair(0L, 0L)
             val vertices = instructions.map {
                 val newVertex = when (it.direction) {
                     'U' -> Pair(curVertex.first-it.distance, curVertex.second)
@@ -30,7 +43,7 @@ class Day18 {
                 curVertex = newVertex
                 newVertex
             }.dropLast(1)
-            var area = 0
+            var area = 0L
             for (i in 0..vertices.size-2) {
                 area += vertices[i].first*vertices[i+1].second - vertices[i+1].first*vertices[i].second
             }
